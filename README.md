58cee260c9ef222f3e41aaed8a74ec22e1100ad9
@victor.ogbebor <victor.ogbebor@simbachain.com>
2022-03-22 18:25:35 -0500

    3-22-22..

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # LearnSolidity
 
-- yo
+- Try all thes out in remix 
+https://remix.ethereum.org/
--- SoildityBasics/004-Functions.sol
+++ SoildityBasics/004-Functions.sol
@@ -11,6 +11,14 @@ contract FunctionsInSolidlity {
     string public valueC;
     int internal valueD;
 
+    address public owner;
+
+    // function modifier => this sets a function to have certain contions met to execute. Basically amkes the function Special 
+    modifier iOwnThis { 
+        require(msg.sender == owner);
+        _;
+    }
+
 
     function setFunc1(uint _valueA) public { // This is considered a setter function...similar to a constructor
         valueA = _valueA;
@@ -29,29 +37,45 @@ contract FunctionsInSolidlity {
     function setFunc3(string memory _valueC) external { 
         valueC = _valueC;
     }
-    function returnFunc3() view external returns (string memory) { 
+    function returnFunc3() view external iOwnThis returns (string memory) { 
         return valueC;
     }
 
     function _setFunc4(int _valueD) internal { 
-        valueD = _valueD;
+        valueD = _valueD - 2;
     }
 
-    function _returnFunc4() view internal returns (uint) { 
-        return valueA;
+    function _returnFunc4() view internal returns (int) { 
+        return valueD;
     }
 
     
 /*
-    * KEYWORDS USED 
+    Explaining Functions in depth 
 
-    - public: Function is able to be called by anywhere
-    - view: 
+    function FunctionName([parameters]) {public|private|internal|external}
+        [pure|view|payable] [modifiers] [returns (return types)]
+
+        FunctionName = Name of the functuoin that will be called from another contract
+        parameters = arguments that are passed to the functions
+            (type nameOfParms)
+
+    * KEYWORDS USED  for visablity
+
+    - public: Function is available by default and xan be called all over|| Variable ...
+    - private: Function can only called inside the contract...no where else || Variable ...
+    - external: Function is similar to a public function, just cannot be called with in the contract
+    - internal: Function is similar to private...function can be called within the contract...o where elses || Variable ...
+
+    - view:
+    - pure:
+    - payable: A payable function is one that can accept incoming payments...Usually depositFunctions
+    
     - returns (return Type): return type = (bool/string/uint/int)
     - return: the variable that matches the return type
 
     - payable: 
-    - external: 
+
  */
     
 
